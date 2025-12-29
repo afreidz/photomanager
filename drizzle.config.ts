5730194e17f98780edbd369bd05ea48b18ab77d1
Andy Freidenfelds <andy.freidenfelds@gmail.com>
2025-12-29 16:39:14 -0600

    add breakpoints to drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,7 @@ export default {
   schema: './src/lib/db/schema.ts',
   out: './drizzle',
   dialect: 'turso',
+  breakpoints: true,
   dbCredentials: {
     url: process.env.DATABASE_URL!,
     authToken: process.env.DATABASE_TOKEN,
